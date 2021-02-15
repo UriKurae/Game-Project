515548f0f1d6c619c65c7d4b933ac4ea5a581c70
UriKurae <youtfrost@gmail.com>
2021-02-15 15:56:14 +0100

    Initial Commit

---
+++ GranProx/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float rotateSpeed;
+    private Rigidbody body;
+
+    void Start()
+    {
+        body = this.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        HandleInput();
+
+        
+    }
+
+    
+    private void HandleInput()
+    {
+        float translate = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotateSpeed;
+
+        translate *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(new Vector3(0.0f, 0.0f, translate));
+        transform.Rotate(new Vector3(0.0f, rotation, 0.0f));
+    }
+}
